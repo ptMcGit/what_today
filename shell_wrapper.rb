85ced6f36188857a53ffd8d067a7adf4dbebd0c5
Zach H <zdhadgraft@gtcc.edu>
2016-10-15 13:11:24 -0400

    Revised ShellWrapper class and added ShellWrapper tests.

--- shell_wrapper.rb
+++ shell_wrapper.rb
@@ -1,10 +1,10 @@
 class ShellWrapper
 
-  attr_accessor     :working_dir
+  attr_accessor     :working_dir, :preferred_shell
 
   def initialize
     @working_dir = '.'
-    @shell       = ENV['SHELL']
+    @preferred_shell       = ENV['SHELL']
     @aliases   = {}
   end
 
@@ -16,51 +16,68 @@ class ShellWrapper
     shell_command command
   end
 
-  #def working_dir=
-  #end
+  def working_dir= dir
+    if File.exist?(dir)
+      @working_dir = dir
+    else
+      raise StandardError.new("argument to directory assignemnt is invalid")
+    end
+  end
+
+  def preferred_shell= shell
+    if is_valid_shell? shell
+      @preferred_shell = shell
+    else
+      raise StandardError.new("argument to shell assignment is invalid")
+    end
+  end
 
   def shell_alias alias_name, command
     @aliases[alias_name.to_sym] = [:shell_command, command]
   end
 
   def exec_alias alias_name, command
-    binding.pry
     @aliases[alias_name.to_sym] = [:exec_command, command]
   end
 
-  def process_alias array
-    self.send(array[0], array[1])
+  def process_alias array, *args
+    self.send(
+      array[0],
+      %{#{array[1]} "#{args.join(" ")}"}
+    )
   end
 
-  def method_missing (method, *args, &block)
-    if m = @aliases[method]
-      process_alias m
-    else
-      raise NoMethodError
-    end
-  end
-
-  def respond_to_missing? method
-    binding.pry
-  end
-  # start a new shell in the given directory (is a child)
+ def method_missing (method, *args, &block)
+   if m = @aliases[method]
+     process_alias(m, *args)
+   else
+     raise NoMethodError
+   end
+ end
 
   def new_shell
-    exec_command "exec #{@shell} -l"
+    exec_command "exec #{@preferred_shell} -l"
   end
 
   private
 
   def exec_command command
-    %x[ #{cd} && #{@shell} -c '#{command}' ]
+    %x[ #{cd} && #{@preferred_shell} -c '#{command}' ]
   end
 
   def shell_command command
-    system( "#{cd} && #{@shell} -c '#{command}'" )
+    system( "#{cd} && #{@preferred_shell} -c '#{command}'" )
   end
 
   def cd
     "cd #{@working_dir}"
+  end
+
+  def is_valid_shell? shell
+    [
+      "/bin/bash",
+      "/bin/sh"
+    ].include? shell
   end
 
 end
---
+++ test/shell_wrapper_test.rb
@@ -0,0 +1,64 @@
+require_relative './test_helper.rb'
+
+class ShellWrapperTests < MiniTest::Test
+
+  def test_can_use_exec_shell
+    s = ShellWrapper.new
+    r = s.exec 'echo "hello world"'
+    assert_equal "hello world", r.chomp
+  end
+
+  def test_can_use_regular_shell
+    s = ShellWrapper.new
+    r = s.exec 'echo "hello world"'
+    assert r
+  end
+
+  def test_can_create_exec_alias
+    s = ShellWrapper.new
+    s.exec_alias( "echo", 'echo' )
+    r = s.echo "hello world"
+    assert_equal "hello world", r.chomp
+  end
+
+  def test_can_create_shell_alias
+    s = ShellWrapper.new
+    s.shell_alias( "echo", 'echo' )
+    r = s.echo "hello world"
+    assert r
+  end
+
+  def test_can_change_dir
+    s = ShellWrapper.new
+    dir = File.expand_path("../")
+    s.working_dir = "../"
+    working_dir = s.exec('pwd').chomp
+    assert_equal working_dir, dir
+  end
+
+  def test_cannot_use_nonexistent_dir
+    s = ShellWrapper.new
+    dir = "./nonexistent_dir"
+    refute File.exist?(dir)
+    assert_raises("directory is non-existent") {
+      s.working_dir = dir
+    }
+  end
+
+  def test_can_change_shell
+    s = ShellWrapper.new
+    shell = '/bin/sh'
+    s.preferred_shell = shell
+    assert_equal s.preferred_shell, shell
+  end
+
+  def test_cannot_change_shell_to_invalid_shell
+    s = ShellWrapper.new
+    shell = '/bin/something'
+    refute File.exist?(shell)
+    assert_raises("argument to shell assignment is invalid") {
+      s.preferred_shell = shell
+    }
+  end
+
+end
