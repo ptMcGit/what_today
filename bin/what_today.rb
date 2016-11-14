7990dae918c8c650697a88d84e78d12da0da2746
Zach H <zdhadgraft@gtcc.edu>
2016-11-14 12:47:29 -0500

    WIP: can cycle through desired repos; enter prompts.

--- bin/what_today.rb
+++ bin/what_today.rb
@@ -1,54 +1,42 @@
 #!/usr/bin/env ruby
 
 require 'pry'
-require './file_finder.rb'
-require './criteria.rb'
-require './git_wrapper.rb'
-require './helpers.rb'
-require './shell_wrapper_parent.rb'
-require './shell_wrapper.rb'
-
-require './test_files/test_repos.rb'
-
-include GitWrapper
-include Helpers
+require_relative '../lib/what_today/criteria.rb'
+require_relative '../lib/what_today/git_wrapper.rb'
+require_relative '../lib/what_today/shell_wrapper.rb'
+require_relative '../lib/what_today/command_wrapper.rb'
+require_relative '../lib/what_today/file_finder.rb'
 
 def debug
   binding.pry
   exit
 end
 
-begin
-  search_critera = Criteria.for
-rescue Criteria::FileNotFoundError => e
-  puts "File not found. Using defaults..."
-  sleep 1
-  search_criteria = Criteria.new
-end
+criteria = WhatToday::Criteria.new
+git_repos = WhatToday::FileFinder.new(criteria.to_h)
 
-#finder = FileFinder.new(search_criteria.prepare)
-#repos = process_directories(finder.results)
-binding.pry
+git_shell = WhatToday::CommandWrapper.shell_aliases_for WhatToday::GitWrapper
 
-shell = ShellWrapper.new
+ignore_directory = lambda { |repo| criteria.ignore_repo? repo }
+contains_git_dir = lambda { |repo| not Dir.glob(repo + '/.*').map { |d| File.basename d }.include? '.git' }
+is_an_up_to_date_git_repo = lambda { |repo| criteria.ignore_up_to_date && ( git_shell.exec("cd #{repo} && git status -s") == '' ) }
 
-repos.each do |r|
+git_repos.add_skip_path_method ignore_directory
+git_repos.add_skip_path_method contains_git_dir
+git_repos.add_skip_path_method is_an_up_to_date_git_repo
+
+git_repos.files.each do |r|
   system( "clear" )
 
-  shell.working_dir = r
+  git_shell.working_dir = r
 
-  next if
-    search_criteria.ignore_updated_repos &&
-    shell.exec(git_status_ok?).empty?
-
-
-  shell.shell git_status
-
+  git_shell.git_status
   print "\nVisit repo at #{r} (y/n)? "
 
   response = gets.chomp
   next unless ["y","Y"].include?(response)
-
-  shell.new_shell
+  puts "Starting #{git_shell.preferred_shell} session... You will need to exit before continuing to next repo."
+  sleep 1
+  WhatToday::ShellWrapper.new_shell(working_dir: r)
 
 end
