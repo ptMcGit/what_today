a4381a1b694da3bb9855423850c9aec51e6e53ad
Zach H <zdhadgraft@gtcc.edu>
2016-11-13 21:30:24 -0500

    Refactored can_add_skip_path_condition FileFinder test.

--- test/file_finder_test.rb
+++ test/file_finder_test.rb
@@ -59,13 +59,13 @@ class FileFinderClassTests < MiniTest::Test
     ff = FileFinder.new(start_directory: TEST_DIR_PREFIX, prune_paths: ["/dir/subdir2", "/dir/subdir4"])
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a - ["/dir/subdir2", "/dir/subdir4"]
   end
-
+focus
   def test_can_add_skip_path_condition
     ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
-    ff.add_skip_path_method { |path| File.basename(path) != ".git" }
+    ff.add_skip_path_method %{{ |path| File.basename(path) != \".git\" }}
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a.reject { |path| File.basename(path) != ".git" }
   end
-focus
+
   def test_can_remove_skip_path_condition
     ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
     m = "{ true }"
