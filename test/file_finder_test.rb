71b296a0d2acee49511de66962068459c2622f10
Zach H <zdhadgraft@gtcc.edu>
2016-11-13 22:18:55 -0500

    Added helper method to file_finder_test.

--- test/file_finder_test.rb
+++ test/file_finder_test.rb
@@ -2,24 +2,28 @@ require_relative './test_helper.rb'
 
 class FileFinderClassTests < MiniTest::Test
 
+  def create_ff config={}
+    FileFinder.new({start_directory: TEST_DIR_PREFIX}.merge(config))
+  end
+
   def test_finds_all_test_paths_with_no_pruning
-    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
+    ff = create_ff
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a
   end
 
   def test_prunes_specified_dirs
-    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX, prune_paths: ["/dir/subdir2", "/dir/subdir4"])
+    ff = create_ff(prune_paths: ["/dir/subdir2", "/dir/subdir4"])
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a - ["/dir/subdir2", "/dir/subdir4"]
   end
 
   def test_can_add_skip_path_condition
-    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
+    ff = create_ff
     ff.add_skip_path_method %{{ |path| File.basename(path) != \".git\" }}
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a.reject { |path| File.basename(path) != ".git" }
   end
 
   def test_can_remove_skip_path_condition
-    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
+    ff = create_ff
     m = "{ true }"
     ff.add_skip_path_method m
     assert_equal ff.files.to_a, []
