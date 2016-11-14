90b1703f8b7475f8d600916a8986360c59ecd3dc
Zach H <zdhadgraft@gtcc.edu>
2016-11-13 21:18:03 -0500

    Added test_can_add_skip_path_condition.

--- test/file_finder_test.rb
+++ test/file_finder_test.rb
@@ -43,16 +43,27 @@ class FileFinderClassTests < MiniTest::Test
                    "/dir/subdir1",
                    "/dir/subdir1/.git",
                    "/dir/subdir2",
-                   "/dir/subdir2/.git",
                    "/dir/subdir3",
                    "/dir/subdir3/.git",
                    "/dir/subdir4"
                  ]
   end
-focus
+
   def test_finds_all_test_paths_with_no_pruning
     ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
     assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a
+  end
+
+
+  def test_prunes_specified_dirs
+    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX, prune_paths: ["/dir/subdir2", "/dir/subdir4"])
+    assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a - ["/dir/subdir2", "/dir/subdir4"]
+  end
+focus
+  def test_can_add_skip_path_condition
+    ff = FileFinder.new(start_directory: TEST_DIR_PREFIX)
+    ff.add_skip_path_method { |path| File.basename(path) != ".git" }
+    assert_equal ff.files.to_a, EXPECTED_DIR_ITEMS.to_a.reject { |path| File.basename(path) != ".git" }
   end
 
   def test_finds_mock_repos
